fe69719d85753f656e4f8bcbb016778386617a61
idanazuri <idan.azuri@gmail.com>
2018-12-03 14:20:49 +0200

    adding more files

---
+++ main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import argparse
+import atexit
+
+import utils
+from data_loader import *
+from model import Model
+
+
+def main(mode):
+	model = Model(mode)
+	model_func = model.build_fn()
+	
+	if mode == Model.TRAIN_MODE:
+		train_loader = make_data_loader("train", Config.train.batch_size)
+		model_func(train_loader)
+	elif mode == Model.PREDICT_MODE:
+		model_func(Config.predict.batch_size)
+	else:
+		raise ValueError(f"unknown mode: {Model.mode}")
+
+
+if __name__ == '__main__':
+	
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('--config', type=str, default='config', help='config file name')
+	parser.add_argument('--mode', type=str, default='train', help='Mode (train/test/train_and_evaluate)')
+	args = parser.parse_args()
+	
+	# Print Config setting
+	Config(args.config)
+	print("Config: ", Config)
+	if Config.get("description", None):
+		print("Config Description")
+		for key, value in Config.description.items():
+			print(f" - {key}: {value}")
+	
+	# After terminated Notification to Slack
+	atexit.register(utils.send_message_to_slack, config_name=args.config)
+	
+	main(args.mode)
---
+++ model.py
@@ -0,0 +1,41 @@
+import torch.nn as nn
+import torch.optim as optim
+from hbconfig import Config
+
+from gan import GAN
+
+
+class Model():
+	TRAIN_MODE = "train"
+	EVALUATE_MODE = "evaluate"
+	PREDICT_MODE = "predict"
+	
+	def __init__(self, mode):
+		self.mode = mode
+	
+	def build_fn(self):
+		gan = GAN()
+		
+		if self.mode == self.TRAIN_MODE:
+			criterion = self.build_criterion()
+			d_optimizer, g_optimizer = self.build_optimizers(gan.discriminator, gan.generator)
+			
+			return gan.train_fn(criterion, d_optimizer, g_optimizer)
+		elif self.mode == self.EVALUATE_MODE:
+			return gan.evaluate_fn()
+		elif self.mode == self.PREDICT_MODE:
+			return gan.predict_fn()
+		else:
+			raise ValueError(f"unknown mode: {self.mode}")
+	
+	def build_criterion(self):
+		return nn.BCELoss()  # Binary cross entropy
+	
+	def build_optimizers(self, discriminator, generator):
+		d_optimizer = optim.Adam(discriminator.parameters(), lr=Config.train.d_learning_rate, betas=Config.train.optim_betas)
+		g_optimizer = optim.Adam(generator.parameters(), lr=Config.train.g_learning_rate, betas=Config.train.optim_betas)
+		
+		return d_optimizer, g_optimizer
+	
+	def build_metric(self):
+		pass
